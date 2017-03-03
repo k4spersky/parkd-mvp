ff010f7ab0fa3d6170c623e96369ee23ae181d47
Kacper Kuczkowski <kkuczkowski01@qub.ac.uk>
2017-03-03 20:08:39 +0000

    minor clean up in styling

--- Parkd/app/src/main/java/com/java/user/parkd/Fragment1Activity.java
+++ Parkd/app/src/main/java/com/java/user/parkd/Fragment1Activity.java
@@ -3,15 +3,10 @@ package com.java.user.parkd;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-/**
- * Created by kkuczkowski on 03/03/2017.
- */
 
 public class Fragment1Activity extends Fragment implements View.OnClickListener {
 
@@ -19,9 +14,11 @@ public class Fragment1Activity extends Fragment implements View.OnClickListener 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         view = inflater.inflate(R.layout.activity_fragment1, container, false);
 
+        //This Listener listens for click on the login button
+        //The login button is located here just for testing purposes
+        //The following code is standard for running a new activity
         Button testButton = (Button) view.findViewById(R.id.button2);
         testButton.setOnClickListener(this);
         return view;
@@ -31,6 +28,5 @@ public class Fragment1Activity extends Fragment implements View.OnClickListener 
     public void onClick(View v) {
         Intent LoginIntent = new Intent(Fragment1Activity.this.getActivity(), LoginActivity.class);
         Fragment1Activity.this.startActivity(LoginIntent);
-
     }
 }
--- Parkd/app/src/main/java/com/java/user/parkd/Fragment3Activity.java
+++ Parkd/app/src/main/java/com/java/user/parkd/Fragment3Activity.java
@@ -2,15 +2,9 @@ package com.java.user.parkd;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-/**
- * Created by kkuczkowski on 03/03/2017.
- */
 
 public class Fragment3Activity extends Fragment {
 
