fc4544246717c34b6f29d3a4b440c5bee9772e1f
PJohnston94 <pjohnston37@qub.ac.uk>
2017-03-06 20:14:51 +0000

    Register UI changes and functions
    Email and validation working

--- Parkd/app-php/login.php
+++ Parkd/app-php/login.php
@@ -3,25 +3,26 @@
     
     $username = $_POST["username"];
     $password = $_POST["password"];
-    
-    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
-    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ?");
+    mysqli_stmt_bind_param($statement, "s", $username);
     mysqli_stmt_execute($statement);
-    
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $userID, $firstname, $lastname, $username, $password, $email);
+    mysqli_stmt_bind_result($statement, $userID, $firstname, $lastname, $username, $colpassword, $email);
     
     $response = array();
-    $response["success"] = false;  
+    $response["success"] = false;
+
     
     while(mysqli_stmt_fetch($statement)){
-        $response["success"] = true;  
-        $response["firstname"] = $firstname;
-        $response["lastname"] = $lastname;
-        $response["username"] = $username;
-        $response["password"] = $password;
-       $response["email"] = $email;
-    }
+    if(password_verify($password, $colpassword)){
+            $response["success"] = true;
+            $response["firstname"] = $firstname;
+            $response["lastname"] = $lastname;
+            $response["username"] = $username;
+            $response["password"] = $password;
+           $response["email"] = $email;
+           }
+            }
     
     echo json_encode($response);
 ?>
