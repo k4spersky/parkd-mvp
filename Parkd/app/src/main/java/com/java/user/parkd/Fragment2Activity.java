3ab621ff176e5b377331cb859e2bb2ed0b92f4e4
Kacper Kuczkowski <kkuczkowski01@qub.ac.uk>
2017-04-20 13:44:34 +0100

    adding sliding up panel logic and styles

--- Parkd/app/src/main/java/com/java/user/parkd/Fragment2Activity.java
+++ Parkd/app/src/main/java/com/java/user/parkd/Fragment2Activity.java
@@ -1,12 +1,23 @@
 package com.java.user.parkd;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+import android.support.v4.widget.SlidingPaneLayout;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -20,12 +31,16 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-import static android.content.ContentValues.TAG;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Fragment2Activity extends Fragment implements OnMapReadyCallback {
 
+    private static final String TAG = "DemoActivity";
+    private SlidingUpPanelLayout mLayout;
     GoogleMap mMap;
     View view;
 
@@ -74,6 +89,77 @@ public class Fragment2Activity extends Fragment implements OnMapReadyCallback {
                 Log.i(TAG, "An error occurred: " + status);
             }
         });
+
+        /*
+         *  SLIDING UP PANEL
+         */
+        ListView lv = (ListView) view.findViewById(R.id.list);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), "onItemClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        List<String> your_array_list = Arrays.asList(
+                "This",
+                "Is",
+                "An",
+                "Example",
+                "ListView"
+        );
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        lv.setAdapter(arrayAdapter);
+
+        mLayout = (SlidingUpPanelLayout) view.findViewById(R.id.sliding_layout);
+
+        //change layout state to hidden for marker on click, like below
+        //mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState,
+                                            SlidingUpPanelLayout.PanelState newState) {
+                //TODO
+            }
+        });
+
+        mLayout.setFadeOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+
+        TextView t = (TextView) view.findViewById(R.id.name);
+        t.setText("Car Park name");
+
+        Button f = (Button) view.findViewById(R.id.follow);
+        f.setMovementMethod(LinkMovementMethod.getInstance());
+        f.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
         return view;
     }
 
