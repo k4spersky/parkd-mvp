0a670c651932d12da2248f40850e6c360126a91e
Kacper Kuczkowski <kkuczkowski01@qub.ac.uk>
2017-04-30 00:16:58 +0100

    removing now redundant panelState call

--- Parkd/app/src/main/java/com/java/user/parkd/Fragment2Activity.java
+++ Parkd/app/src/main/java/com/java/user/parkd/Fragment2Activity.java
@@ -156,9 +156,6 @@ public class Fragment2Activity extends Fragment implements OnMapReadyCallback {
          */
         mLayout = (SlidingUpPanelLayout) view.findViewById(R.id.sliding_layout);
 
-        //set screen height
-        mLayout.setAnchorPoint(0.5f);
-
         //change layout state to hidden for marker on click, like below
         mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
 
