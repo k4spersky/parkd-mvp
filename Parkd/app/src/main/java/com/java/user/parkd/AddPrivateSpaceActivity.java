cdb993634cb8c30c4f81e1703a84c94c1618d74d
Kacper Kuczkowski <kkuczkowski01@qub.ac.uk>
2017-05-03 15:38:17 +0100

    final design and logic

--- Parkd/app/src/main/java/com/java/user/parkd/AddPrivateSpaceActivity.java
+++ Parkd/app/src/main/java/com/java/user/parkd/AddPrivateSpaceActivity.java
@@ -3,16 +3,22 @@ package com.java.user.parkd;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 
 /**
  * desc
  */
-
 public class AddPrivateSpaceActivity extends AppCompatActivity {
     Toolbar tb1;
+    Button addSpaceBtn;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +27,27 @@ public class AddPrivateSpaceActivity extends AppCompatActivity {
         setSupportActionBar(tb1);
         getSupportActionBar().setTitle("Add new Private Space");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        addSpaceBtn = (Button) findViewById(R.id.addSpaceBtn);
 
+        // blue spinner (city picker)
+        Spinner spaceSpinner = (Spinner) findViewById(R.id.space_type_spinner);
+        String[] spaceTypes = new String[] {"Private", "Other"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, spaceTypes);
+
+        spaceSpinner.setAdapter(adapter);
+        spaceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 
     @Override
@@ -31,5 +57,4 @@ public class AddPrivateSpaceActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
